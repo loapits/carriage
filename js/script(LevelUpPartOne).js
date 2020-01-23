aafabd5310b7d46aa18d0c398525658696407be7
gera <gera59377@gmail.com>
2020-01-24 01:53:10 +0600

    fix html

--- js/script(LevelUpPartOne).js
+++ js/script(LevelUpPartOne).js
@@ -1,10 +1,10 @@
 songe = [
-	muz_one = [0, 'Carriage - Интродакшн', 'media/LVLUP/Introducthion.mp3','141', 'img/covers/LEVEL UP.png'],
-	muz_two = [1, 'Carriage - Второй Трек На Альбоме', 'media/LVLUP/Second_track_for_album.mp3','252.5', 'img/covers/LEVEL UP.png'],
-	muz_free = [2, 'Carriage - Всё ещё накручиваешь себя', 'media/LVLUP/Still_winding_yourself.mp3','110.2', 'img/covers/LEVEL UP.png'],
-	muz_four = [3, 'Carriage - Кстати, ненависти не существует', 'media/LVLUP/By_the_way_hatred_does_not_exist.mp3','273.5', 'img/covers/LEVEL UP.png'],
-	muz_five = [4, 'Carriage - Одни дожди опять', 'media/LVLUP/Some_rains_again.mp3','169.5', 'img/covers/LEVEL UP.png'],
-	muz_six = [5, 'Carriage - Варп (Opale remix)', 'media/LVLUP/Warp.mp3','170.5', 'img/covers/LEVEL UP.png'],
-	muz_seven = [6, 'Carriage - Утверждение или Восклицание', 'media/LVLUP/Approval_or_Exclamation.mp3','222', 'img/covers/LEVEL UP.png'],
-	muz_eight = [7, 'Carriage - Away feat. Quallm', 'media/LVLUP/Away_feat_Quallm.mp3','165.5', 'img/covers/LEVEL UP.png']
+	muz_one = [0, 'Carriage - Интродакшн', 'media/LVLUP/Introducthion.mp3','141', 'img/covers/LEVELUP.png'],
+	muz_two = [1, 'Carriage - Второй Трек На Альбоме', 'media/LVLUP/Second_track_for_album.mp3','252.5', 'img/covers/LEVELUP.png'],
+	muz_free = [2, 'Carriage - Всё ещё накручиваешь себя', 'media/LVLUP/Still_winding_yourself.mp3','110.2', 'img/covers/LEVELUP.png'],
+	muz_four = [3, 'Carriage - Кстати, ненависти не существует', 'media/LVLUP/By_the_way_hatred_does_not_exist.mp3','273.5', 'img/covers/LEVELUP.png'],
+	muz_five = [4, 'Carriage - Одни дожди опять', 'media/LVLUP/Some_rains_again.mp3','169.5', 'img/covers/LEVELUP.png'],
+	muz_six = [5, 'Carriage - Варп (Opale remix)', 'media/LVLUP/Warp.mp3','170.5', 'img/covers/LEVELUP.png'],
+	muz_seven = [6, 'Carriage - Утверждение или Восклицание', 'media/LVLUP/Approval_or_Exclamation.mp3','222', 'img/covers/LEVELUP.png'],
+	muz_eight = [7, 'Carriage - Away feat. Quallm', 'media/LVLUP/Away_feat_Quallm.mp3','165.5', 'img/covers/LEVELUP.png']
 ];
