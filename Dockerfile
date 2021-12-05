caaf9faf3f303f6f87fb7c4ef7da70d559379402
loapits <gera59377@gmail.com>
2021-12-06 03:24:15 +0600

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:16.13.1-alpine as build
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm ci
+COPY . ./
+RUN npm run start
+
+FROM nginx:stable-alpine
+COPY ./ /usr/share/nginx/html
+COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express')
-//  const favicon = require('express-favicon');
  const path = require('path')
  const port = process.env.PORT || 8080
  
- // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
  const app = express()
-//  app.use(favicon(__dirname + '/build/favicon.png')); 
  
-//здесь наше приложение отдаёт статику
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname)))
 
-//простой тест сервера
 app.get('/ping', (req, res) => {
   return res.send('pong');
 })
 
-//обслуживание html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
